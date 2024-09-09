b320527f87cb95957f218b3f3855d9c2379b8e51
Douglas Zody <douglaszody@Douglass-Air.attlocal.net>
2024-09-08 19:06:31 -0700

    update

--- src/screens/home/FavoritesScreen.tsx
+++ src/screens/home/FavoritesScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const FavoritesScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profile Screen</Text>
+      <Text style={styles.text}>Favorites Screen</Text>
     </View>
   );
 };
